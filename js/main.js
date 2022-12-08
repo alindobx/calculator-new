902a5464262259e2e08c2b8a1cd1da953cfcff5f
zitechlo <tonyromebx@gmail.com>
2022-12-08 13:39:14 -0500

    version 3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //getValue of Digits
-const leftPos = document.querySelector('.leftPos');
+const leftOperand = document.querySelector('.leftOperand');
 const rightPos = document.querySelector('.rightPos');
 const getSum = document.querySelector('.sum');
 const getSymbols = document.querySelectorAll('.symbol');
@@ -8,17 +8,18 @@ const equalBtn = document.getElementById('equal');
 const getOperator = document.querySelector('.operator');
 const clearAll = document.getElementById('clearAll');
 
-let sumTotal = [];
 let addTotal = [];
+let leftOperandArr = [];
+const previousOperand = [];
+let operatorArr =[];
 let clickCount = 0;
-console.log(sumTotal);
 
 //clear function
 clearAll.addEventListener("click", ()=>{
     for (let x of selectDigit) {
         x.classList.remove('noClick');
     }
-    leftPos.innerHTML = "0";
+    leftOperand.innerHTML = "0";
     rightPos.innerHTML = "";
     getSum.innerHTML = '';
     clickCount = 0;
@@ -26,21 +27,34 @@ clearAll.addEventListener("click", ()=>{
 })
 
 //get you digits function
-const getDigitValue = () => {
+const getOperand = () => {
     selectDigit.forEach(digit =>{
         digit.addEventListener('click',(e) =>{
             const value = parseInt(e.target.innerHTML, 10);
             getSum.innerHTML = '';
-            addTotal.push(value);
-            console.log(addTotal);
-            console.log(sumTotal.push(value));
-            if(sumTotal.length === 1){
-                leftPos.innerHTML = `${value}`
+            //checks to see if the value is a number or not
+            if(isNaN(value) === true) {
+                const total = leftOperandArr.join('')//join elements in array
+                previousOperand.push(total);
+                console.log('this is the value',value)
+                leftOperand.innerHTML = '';
+                leftOperandArr = [];
+                switch(value) {
+                    case '+':
+                        operatorArr.push(value);
+                        console.log('this is the +',value)
+                        break;
+                    case'-':
+                        operatorArr.push(value);
+                        console.log('this is the -',value)
+                        break;
+                }
             }else{
-                rightPos.append(`${value}`)
+                leftOperandArr.push(value);
+                leftOperand.append(`${value}`);
+                console.log('pushed to array')
             }
-            clickCount++
-            console.log("clickCount",clickCount)
+
             if(clickCount > 10) {
                console.log("pass 10")
                 for (let x of selectDigit){
@@ -50,32 +64,37 @@ const getDigitValue = () => {
         })
     })
 }
-getDigitValue();
+getOperand();
 //Equation Button
-equalBtn.addEventListener("click",()=>{
-    const totalSum = eval(sumTotal[0] + sumTotal[1] + sumTotal[2])
-    getSum.innerHTML = `${totalSum}`
-    leftPos.innerHTML = '';
-    getOperator.innerHTML = ''
-    rightPos.innerHTML = '';
-    console.log('= button pressed');
-    console.log(totalSum);
-    sumTotal = [];
-})
+// equalBtn.addEventListener("click",()=>{
+//     const secondGrab = leftOperand.innerHTML;
+//     addTotal.push(`${secondGrab}`);
+//     let operand1 = parseInt(addTotal[0],10) ;
+//     let operand2 = parseInt(addTotal[1], 10);
+//     const finalTotal =  operand1 + operand2;
+//      console.log(finalTotal);
+//     console.log('test',operand1 + 1);
+//     leftOperand.innerHTML = `${finalTotal}`
+// })
 //click Operator Buttons
-getSymbols.forEach(sym => {
-    sym.addEventListener('click',(e)=>{
-        switch(e.target.innerHTML) {
-            case "+":
-                sumTotal.push('+')
-                console.log("The operator is currently","+")
-                getOperator.innerHTML = '+';
-                break;
-            case "-":
-                sumTotal.push('-')
-                getOperator.innerHTML = '-';
-                console.log("The operator is currently","-")
-        }
-    })
-});
+// getSymbols.forEach(sym => {
+//     sym.addEventListener('click',(e)=>{
+//         const valueCnt = leftOperand.innerHTML;
+//         switch(e.target.innerHTML) {
+//             case "+":
+//                 addTotal.push(valueCnt);
+//                 if(leftOperand.innerHTML === ''){
+//                     console.log('empty')
+//                 }else{
+//
+//                 }
+//                 console.log("The operator is currently","+")
+//                 console.log(addTotal)
+//                 break;
+//             case "-":
+//                 addTotal.push(valueCnt)
+//                 console.log("The operator is currently","-")
+//         }
+//     })
+// });
 
